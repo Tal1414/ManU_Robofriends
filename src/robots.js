d7de98f7cb4b7698dac7aee14004aa56ac468f17
Tal <Tal1414@gmail.com>
2018-10-08 16:09:00 +0300

    First commit
    REACT

---
+++ src/robots.js
@@ -0,0 +1,55 @@
+export const robots = [
+{
+	id:1 ,
+	name:'Anthony' ,
+	username:'TM11' ,
+	email: 'TonyMartial@Gmail.com'
+},
+{
+	id:2 ,
+	name:'Jesse' ,
+	username:'JLingz' ,
+	email: 'JLingz@Gmail.com'
+},
+{
+	id:3 ,
+	name:'Paul' ,
+	username:'PP6' ,
+	email: 'PaulPogba@Gmail.com'
+},
+{
+	id:4 ,
+	name:'David',
+	username:'DDG' ,
+	email: 'DDG@Gmail.com'
+},
+{
+	id:5 ,
+	name:'Jose' ,
+	username:'JM1' ,
+	email: 'JoseMourinho@Gmail.com'
+},
+{
+	id:6 ,
+	name:'Juan',
+	username:'JM8' ,
+	email: 'TheSpecialJuan@Gmail.com'
+},
+{
+	id:7 ,
+	name:'Nemanja',
+	username:'Matic' ,
+	email: 'NemanjaMatic@Gmail.com'
+},
+{
+	id:8 ,
+	name:'Antonio',
+	username:'AV25' ,
+	email: 'AntonioV@Gmail.com'
+},
+{
+	id:9 ,
+	name:'Luke',
+	username:'Luke' ,
+	email: 'LukeShaw@Gmail.com'
+}];
