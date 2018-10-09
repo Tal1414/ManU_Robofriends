2ddc6c9b02d2d2dc8ae2d168e526ba701b0c1f95
Tal <Tal1414@gmail.com>
2018-10-09 10:51:36 +0300

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(robotsData => this.setState({robots: robotsData}));
 	}
 
 onSearchChange = (event) => { 
@@ -18,8 +23,8 @@ onSearchChange = (event) => {
 }
 
 render() {
-	const filteredRobots = this.state.robots.filter(robots => {
-		return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+	const filteredRobots = this.state.robots.filter(robot => {
+		return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 
 	});
 
